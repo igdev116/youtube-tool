e1b69dc667f70cc9fdf102a36c766e7cec0b1fce
igdev116 <minhhunghuynh1106@gmail.com>
2025-07-27 13:51:19 +0700

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,10 +8,10 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
     credentials: true,
   });
-  // const port = process.env.PORT ?? 4000;
-  await app.listen(4000);
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port);
 
-  console.log(`🚀 Server is running on http://localhost:4000`);
+  console.log(`🚀 Server is running on http://localhost:${port}`);
   console.log(
     `📱 Telegram Bot Token: ${process.env.TELEGRAM_BOT_TOKEN ? 'Loaded ✅' : 'Missing ❌'}`,
   );
